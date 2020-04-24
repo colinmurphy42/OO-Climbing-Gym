4627721c49a5cf1ab3f8ab4e6f8c5e3a247a296f
Oscar Delgado <oscardel413@gmail.com>
2020-04-24 15:34:35 -0600

    OOGym edits

--- OOGym.py
+++ OOGym.py
@@ -5,16 +5,17 @@ class OOGym:
         self.db = DB.DB()
         self.dailyCust = []
 
-    def checkIn(phone): # Might add all chekcing in processes here to connect UI
-    pass
+    # def checkIn(phone): # Might add all chekcing in processes here to connect UI
+    # pass
     #     memObj = self.db.getMember(phone)
-    #     checkObj = checkingIn.checkingIn(mem)
+    #     checkObj = checkingIn.checkingIn(memObj)
     # if (shoes == True):
     #     checkObj = checkingIn.Shoes(checkObj, memObj)
     # if (Rope == True):
-    #     checkObj = checkingIn.Shoes(checkObj, memObj)
+    #     checkObj = checkingIn.Rope(checkObj, memObj)
     # if (Harness == True):
-    #     checkObj = checkingIn.Shoes(checkObj, memObj)
+    #     checkObj = checkingIn.Harness(checkObj, memObj)
+    #
     # rec = {'date' : datetime.now() - timedelta(hours=7)}
     # rec.update(checkObj.checkout())
     # self.dailyCust.append(rec)
@@ -26,3 +27,7 @@ class OOGym:
 
     def closing():
         data = {'date' : datetime.now() - timedelta(hours=7) , 'dailyReceipts' : self.dailyCust}
+
+    def adduser(self, name, phone , M,C):
+        self.db.addUser(name, phone, M , C)
+        return ("User: " + name + " added! :)")
